56b574ad9b5b7a64de655a2130dd93d2655ec96b
Xavier <xaviier2922@gmail.com>
2020-10-30 08:43:59 +0000

    Prova de edicio amb servidor WEB

--- src/enfonsarlaflota/Aigua.java
+++ src/enfonsarlaflota/Aigua.java
@@ -4,7 +4,7 @@ public class Aigua extends Fitxa{
     
     private String valor;
     
-    public Aigua(String valor, int longitud, int fitxes) {
+    public Aigua(String valooor, int longitud, int fitxes) {
         super(valor, longitud, fitxes);
     }
 
